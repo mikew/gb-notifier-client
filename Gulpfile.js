79ff2aad890e5a50bfa6ca94b882eff36e4cfe65
Mike Wyatt <wyatt.mike@gmail.com>
2016-03-10 12:33:24 -0400

    better way of using electron-builder

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,2 +1,15 @@
 var Parched = require('parched')
 Parched.setup(require('./ParchedConfig'))
+
+var gulp = require('gulp')
+var buildPromise = require('electron-builder/out/builder').build
+
+gulp.task('build-electron', 'Build and package Electron app', (done) => {
+  return buildPromise({
+    appDir: 'public',
+    out: 'dist',
+    dist: true,
+    platform: ['all'],
+    arch: 'all',
+  })
+})
